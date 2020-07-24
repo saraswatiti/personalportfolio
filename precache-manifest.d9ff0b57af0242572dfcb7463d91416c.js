0c3799c421d249ebf978c9c12a077d9c1a6195ce
saraswatiti <saraswati.rigorousweb@gmail.com>
2020-07-24 18:08:10 +0545

    Updates

---
+++ precache-manifest.d9ff0b57af0242572dfcb7463d91416c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76832c422e6fecb222982a784be5ea5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adb6596b76e53028de42",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ee32ba05746ea09718c2",
+    "url": "/static/css/main.38939fe6.chunk.css"
+  },
+  {
+    "revision": "adb6596b76e53028de42",
+    "url": "/static/js/2.c451a18f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c451a18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee32ba05746ea09718c2",
+    "url": "/static/js/main.d9ee13de.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  },
+  {
+    "revision": "81899e18586b10083308cdf1270c656f",
+    "url": "/static/media/avataaars.81899e18.svg"
+  }
+]);
