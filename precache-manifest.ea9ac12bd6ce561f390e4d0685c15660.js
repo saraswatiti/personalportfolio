fa7a3a6ad3861d5e4afd21fbbf1073b76e6d77ab
saraswatiti <saraswati.rigorousweb@gmail.com>
2020-07-27 18:02:43 +0545

    Updates

---
+++ precache-manifest.ea9ac12bd6ce561f390e4d0685c15660.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6741651b5df44e2de8ffa672e1669853",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1f2cedc14600cadefeed",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e8e71d6baf3b75197b38",
+    "url": "./static/css/main.38939fe6.chunk.css"
+  },
+  {
+    "revision": "1f2cedc14600cadefeed",
+    "url": "./static/js/2.124dbf30.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.124dbf30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8e71d6baf3b75197b38",
+    "url": "./static/js/main.bce0117f.chunk.js"
+  },
+  {
+    "revision": "1436ac51eb3085ff3d5c",
+    "url": "./static/js/runtime-main.c2538a3a.js"
+  },
+  {
+    "revision": "81899e18586b10083308cdf1270c656f",
+    "url": "./static/media/avataaars.81899e18.svg"
+  },
+  {
+    "revision": "56ec4ee72a91e8f3d954db6cccbf8ab2",
+    "url": "./static/media/cabin.56ec4ee7.png"
+  },
+  {
+    "revision": "a29c0b43958bf3638364e15ff646fffa",
+    "url": "./static/media/cake.a29c0b43.png"
+  },
+  {
+    "revision": "e27c88e555910ddce50b6f1e275023a4",
+    "url": "./static/media/circus.e27c88e5.png"
+  },
+  {
+    "revision": "107d31e81b23d07ad38c7cc4d1f7ea27",
+    "url": "./static/media/game.107d31e8.png"
+  },
+  {
+    "revision": "0b8ad7942d8c1a70295a9c7b0b2e58e3",
+    "url": "./static/media/repository-open-graph-template.0b8ad794.png"
+  },
+  {
+    "revision": "7dfdcf559d331a0cdc05cc1d2b8c8233",
+    "url": "./static/media/safe.7dfdcf55.png"
+  },
+  {
+    "revision": "8fad4ff92040b17345896e0e777a79ad",
+    "url": "./static/media/submarine.8fad4ff9.png"
+  }
+]);
